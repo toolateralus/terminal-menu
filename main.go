1f88c1133187fb634aa358df8dc0fe9c51e14441
toolateralus <joshnjoshgaming@gmail.com>
2024-05-02 13:29:15 -0400

    add page up and down

--- main.go
+++ main.go
@@ -31,49 +31,48 @@ func NewState(previous *State, path string) State {
 			log.Fatal(err)
 		}
 		items := []string{}
-		
+
 		for _, file := range files {
 			// try hide the hidden files.
 			if !strings.HasPrefix(file.Name(), ".") {
-					fileInfo, err := os.Stat(filepath.Join(path, file.Name()))
-					if err != nil {
-							log.Fatal(err)
-					}
-					// Check if the file has read permission
-					if fileInfo.Mode().Perm()&0400 != 0 {
-							items = append(items, file.Name())
-					}
+				fileInfo, err := os.Stat(filepath.Join(path, file.Name()))
+				if err != nil {
+					log.Fatal(err)
+				}
+				// Check if the file has read permission
+				if fileInfo.Mode().Perm()&0400 != 0 {
+					items = append(items, file.Name())
+				}
 			}
-	}
-		
+		}
+
 		return State{
 			items:    items,
 			selected: 0,
 			running:  true,
 			path:     path,
 			previous: previous,
-			isDir: 		true,
+			isDir:    true,
 		}
 	} else {
 		contents, err := os.ReadFile(path)
 		if err != nil {
 			log.Fatal(err)
 		}
-		
+
 		lines := strings.Split(string(contents), "\n")
-		
+
 		return State{
 			items:    lines,
 			selected: 0,
 			running:  true,
 			path:     path,
 			previous: previous,
-			isDir: 		false,
+			isDir:    false,
 		}
-			
+
 	}
-	
-	
+
 }
 
 type InputHandler struct {
@@ -93,37 +92,25 @@ func termbox_print(x, y int, fg, bg termbox.Attribute, msg string) {
 }
 
 func (renderer *Renderer) Draw() {
-	_, h := termbox.Size() 
+	_, h := termbox.Size()
 	page := renderer.state.selected / h
 	start := page * h
 	end := min(len(renderer.state.items), start+h)
-	
+
 	for i := start; i < end; i++ {
 		item := renderer.state.items[i]
 		if i == renderer.state.selected {
 			termbox_print(0, i-start, termbox.ColorBlack, termbox.ColorWhite, "> "+item)
-			} else {
-				termbox_print(0, i-start, termbox.ColorWhite, termbox.ColorDefault, fmt.Sprintf("%d %s", i+1, item))
-			}
+		} else {
+			termbox_print(0, i-start, termbox.ColorWhite, termbox.ColorDefault, fmt.Sprintf("%d %s", i+1, item))
 		}
-		
-	termbox_print(0, end + 1, termbox.ColorWhite, termbox.ColorGreen, "[esc] to go up, [enter] to go in, [up arrow] to move up, [down arrow] to move down\n")
+	}
+
+	termbox_print(0, end+1, termbox.ColorWhite, termbox.ColorBlack, "[esc] to go up, [enter] to go in, [up arrow] to move up, [down arrow] to move down\n")
+	termbox_print(0, end+2, termbox.ColorLightYellow, termbox.ColorBlack, fmt.Sprintf("%s\n", renderer.state.path))
 	termbox.Flush()
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
 
 func (renderer *Renderer) Clear() {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
@@ -134,12 +121,10 @@ func (handler *InputHandler) OnEnter() {
 		// cant go deeper into a file. only escpae.
 		return
 	}
-	
+
 	selected := handler.state.items[handler.state.selected]
 	path := fmt.Sprintf("%s/%s", handler.state.path, selected)
-	
-	
-	
+
 	oldState := *handler.state
 	*handler.state = NewState(&oldState, path)
 }
@@ -165,6 +150,22 @@ func (handler *InputHandler) OnArrowDown() {
 	}
 }
 
+func (handler *InputHandler) OnPageUp() {
+	_, h := termbox.Size()
+	page := handler.state.selected / h
+	start := page * h
+	end := max(0, start-h)
+	handler.state.selected = end
+	
+}
+func (handler *InputHandler) OnPageDown() {
+	_, h := termbox.Size()
+	page := handler.state.selected / h
+	start := page * h
+	end := min(len(handler.state.items) - 1, start+h)
+	handler.state.selected = end
+}
+
 func (handler *InputHandler) PollInput() {
 	defer func() {
 		handler.inputDone <- true
@@ -183,6 +184,10 @@ func (handler *InputHandler) PollInput() {
 			handler.OnArrowUp()
 		case termbox.KeyArrowDown:
 			handler.OnArrowDown()
+		case termbox.KeyPgdn: 
+			handler.OnPageDown()
+		case termbox.KeyPgup:
+			handler.OnPageUp()
 		}
 	default:
 		break
@@ -201,7 +206,7 @@ func Run(state *State, renderer *Renderer, handler *InputHandler) {
 			renderer.Draw()
 		default:
 		}
-		
+
 		time.Sleep(time.Millisecond * 16)
 	}
 }
@@ -212,19 +217,18 @@ func main() {
 		panic(err)
 	}
 	defer termbox.Close()
-	
+
 	homeDir := os.Getenv("HOME")
 	state := NewState(nil, homeDir)
-	
+
 	handler := &InputHandler{
 		state:     &state,
 		inputDone: make(chan bool),
 	}
-	
+
 	renderer := &Renderer{
 		state: &state,
 	}
-	
-	
+
 	Run(&state, renderer, handler)
 }
