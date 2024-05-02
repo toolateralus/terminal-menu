b3095c90386387450d4229aa9ab1c30d87f96324
toolateralus <joshnjoshgaming@gmail.com>
2024-05-02 13:04:15 -0400

    added directory and file travesal

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/nsf/termbox-go"
@@ -11,6 +14,54 @@ type State struct {
 	items    []string
 	selected int
 	running  bool
+	isDir    bool
+	path     string
+	previous *State
+}
+
+func NewState(previous *State, path string) State {
+	stats, err := os.Stat(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if stats.IsDir() {
+		files, err := os.ReadDir(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		items := []string{}
+		for _, file := range files {
+			items = append(items, file.Name())
+		}
+		
+		return State{
+			items:    items,
+			selected: 0,
+			running:  true,
+			path:     path,
+			previous: previous,
+			isDir: 		true,
+		}
+	} else {
+		contents, err := os.ReadFile(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		
+		lines := strings.Split(string(contents), "\n")
+		
+		return State{
+			items:    lines,
+			selected: 0,
+			running:  true,
+			path:     path,
+			previous: previous,
+			isDir: 		false,
+		}
+			
+	}
+	
+	
 }
 
 type InputHandler struct {
@@ -22,7 +73,7 @@ type Renderer struct {
 	state *State
 }
 
-func tbprint(x, y int, fg, bg termbox.Attribute, msg string) {
+func termbox_print(x, y int, fg, bg termbox.Attribute, msg string) {
 	for _, c := range msg {
 		termbox.SetCell(x, y, c, fg, bg)
 		x++
@@ -32,9 +83,9 @@ func tbprint(x, y int, fg, bg termbox.Attribute, msg string) {
 func (renderer *Renderer) Draw() {
 	for i, item := range renderer.state.items {
 		if i == renderer.state.selected {
-			tbprint(0, i, termbox.ColorBlack, termbox.ColorWhite, "> "+item)
+			termbox_print(0, i, termbox.ColorBlack, termbox.ColorWhite, "> "+item)
 		} else {
-			tbprint(0, i, termbox.ColorWhite, termbox.ColorDefault, fmt.Sprintf("%d %s", i+1, item))
+			termbox_print(0, i, termbox.ColorWhite, termbox.ColorDefault, fmt.Sprintf("%d %s", i+1, item))
 		}
 	}
 	termbox.Flush()
@@ -44,10 +95,25 @@ func (renderer *Renderer) Clear() {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 }
 
-func (handler *InputHandler) OnEnter() {}
+func (handler *InputHandler) OnEnter() {
+	if !handler.state.isDir {
+		// cant go deeper into a file. only escpae.
+		return
+	}
+	
+	selected := handler.state.items[handler.state.selected]
+	path := fmt.Sprintf("%s/%s", handler.state.path, selected)
+	
+	
+	
+	oldState := *handler.state
+	*handler.state = NewState(&oldState, path)
+}
 
 func (handler *InputHandler) OnEscape() {
-	handler.state.running = false
+	if handler.state.previous != nil {
+		*handler.state = *handler.state.previous
+	}
 }
 
 func (handler *InputHandler) OnArrowUp() {
@@ -74,8 +140,9 @@ func (handler *InputHandler) PollInput() {
 	case termbox.EventKey:
 		switch ev.Key {
 		case termbox.KeyEsc:
-		case termbox.KeyCtrlC:
 			handler.OnEscape()
+		case termbox.KeyCtrlC:
+			handler.state.running = false
 		case termbox.KeyEnter:
 			handler.OnEnter()
 		case termbox.KeyArrowUp:
@@ -83,7 +150,6 @@ func (handler *InputHandler) PollInput() {
 		case termbox.KeyArrowDown:
 			handler.OnArrowDown()
 		}
-
 	default:
 		break
 	}
@@ -98,7 +164,7 @@ func Run(state *State, renderer *Renderer, handler *InputHandler) {
 		}
 		renderer.Clear()
 		renderer.Draw()
-		time.Sleep(time.Millisecond * 8)
+		time.Sleep(time.Millisecond * 16)
 	}
 }
 
@@ -109,24 +175,20 @@ func main() {
 	}
 	defer termbox.Close()
 
-	state := &State{
-		items:    []string{"One", "Two", "Three"},
-		selected: 0,
-		running:  true,
-	}
+	state := NewState(nil, "/")
 
 	inputDone := make(chan bool)
 
 	handler := &InputHandler{
-		state:     state,
+		state:     &state,
 		inputDone: inputDone,
 	}
 
 	renderer := &Renderer{
-		state: state,
+		state: &state,
 	}
 
 	go handler.PollInput()
 
-	Run(state, renderer, handler)
+	Run(&state, renderer, handler)
 }
